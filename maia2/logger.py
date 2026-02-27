d4fb3cc9b594ee2f8d8a06af3a29f75c90363eea
Gideon Oba <deyonoba@gmail.com>
2026-02-27 10:39:50 +0100

    Merge 'chess.pgn' library logger with custom project logger

--- maia2/logger.py
+++ maia2/logger.py
@@ -1,12 +1,22 @@
 import logging.config
 import logging
+import pathlib
+
+def setup_log_directory():
+    log_dir_path = pathlib.Path(__file__).parent / "logs"
+    print("Creating logs directory ...")
+    log_dir_path.mkdir(parents=True, exist_ok=True)
+    print(f"Created logs directory: {log_dir_path}")
+    return log_dir_path
+
+log_path = setup_log_directory()
 
 LOGGING_CONFIG = {
     'version': 1,
-    'disable_existing_loggers': False, # Keep existing loggers
+    'disable_existing_loggers': True, # Keep existing loggers
     'formatters': {
         'standard': {
-            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s' #
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s' 
         },
     },
     'handlers': {
@@ -14,17 +24,24 @@ LOGGING_CONFIG = {
             'class': 'logging.StreamHandler', #
             'formatter': 'standard',
         },
-        "file": {
+        "data_file": {
             "class": "logging.handlers.RotatingFileHandler",
-            "filename": "app.log",
+            "filename": log_path / "data.log",
             "formatter": "standard",
-            "maxBytes": 1024,
+            "maxBytes": 10 * 1024 * 1024,
             "backupCount": 3
+        },
+        "file": { # default file handler
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": log_path / "app.log",
+            "formatter": "standard",
+            "maxBytes": 1 * 1024 * 1024,
+            "backupCount": 1
         }
     },
     'loggers': {
         'data': {
-            'handlers': ['file'],
+            'handlers': ['data_file'],
             'level': 'DEBUG',
         },
         'processing': {
@@ -35,6 +52,10 @@ LOGGING_CONFIG = {
             "handlers": ["file", "console"],
             "level": "DEBUG"
         },
+        "chess.pgn": {
+            "handlers": ["data_file"],
+            "level": "DEBUG"
+        }
     }
 }
 
